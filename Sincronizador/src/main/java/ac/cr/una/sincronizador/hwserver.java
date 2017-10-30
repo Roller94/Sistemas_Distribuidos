5476479256b13d0e0a02138fdf6a090d71a99d7d
Osvaldo <osv_93@hotmail.es>
2017-10-29 23:32:35 -0600

    nm

--- Sincronizador/src/main/java/ac/cr/una/sincronizador/hwserver.java
+++ Sincronizador/src/main/java/ac/cr/una/sincronizador/hwserver.java
@@ -1,6 +1,10 @@
 package ac.cr.una.sincronizador;
 
 import org.zeromq.ZMQ;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.zeromq.ZMsg;
 
 public class hwserver {
 
@@ -12,16 +16,29 @@ public class hwserver {
         responder.bind("tcp://*:5555");
 
         while (!Thread.currentThread().isInterrupted()) {
-            // Wait for next request from the client
-            byte[] request = responder.recv(0);
-            System.out.println("Received Hello");
+            
+            ZMsg inMsg = ZMsg.recvMsg(responder);
+            String contentType = inMsg.pop().toString();
+            String fileName = inMsg.pop().toString();
+            byte[] fileData = inMsg.pop().getData();
 
-            // Do some 'work'
-            Thread.sleep(1000);
-
-            // Send reply back to client
-            String reply = "World";
-            responder.send(reply.getBytes(), 0);
+            Files.write(Paths.get("D:\\SistemasDistribuidos\\"+fileName),fileData);
+            
+            
+            
+//            // Wait for next request from the client
+//            byte[] request = responder.recv(0);
+//            System.out.println("Received Hello");
+//
+//            // Do some 'work'
+//            Thread.sleep(1000);
+////            File file = new File("C:\\SistemasDistribuidos");
+//            byte[] array = Files.readAllBytes(new File("C:\\SistemasDistribuidos\\a.txt").toPath());
+//
+//            // Send reply back to client
+////            String reply = "World";
+            responder.send("ok");
+            
         }
         responder.close();
         context.term();
